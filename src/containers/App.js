576831bdaebf092652058700a5ba41f6973c8fdf
M <M@DESKTOP-J0D963F>
2020-08-16 17:56:07 +0300

    final project roboamigo

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(
-        response => {
+        response  => {
           return response.json();
         })
       .then(users => {
