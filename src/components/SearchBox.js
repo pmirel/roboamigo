00db3b467df823c0f8f9b160fd7c0ae12e8ee44f
M <M@DESKTOP-J0D963F>
2020-08-16 17:36:46 +0300

    final project roboamigo

---
+++ src/components/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBox = ({searchChange}) => {
+  return (
+    <div>
+    <input 
+    className='pa3 ba b--green bg-lightest-blue'
+    type='search' placeholder='search for robots'
+    onChange = {searchChange}/>
+    
+    </div>
+  )
+}
+export default SearchBox;
